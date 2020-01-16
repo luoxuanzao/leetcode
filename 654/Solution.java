62f20f2ff2629f381c7ab6f28fb2d4daef537849
lxz <1246399584@qq.com>
2020-01-16 21:47:31 +0800

    1.16

--- 654/Solution.java
+++ 654/Solution.java
@@ -9,6 +9,16 @@ class Solution {
       TreeNode right;
       TreeNode(int x) { val = x; }
   }
+
+    /**
+     * 最大二叉树
+     * 二叉树的根是数组中的最大元素。
+     * 左子树是通过数组中最大值左边部分构造出的最大二叉树。
+     * 右子树是通过数组中最大值右边部分构造出的最大二叉树。
+     *
+     * @param nums
+     * @return
+     */
     public TreeNode constructMaximumBinaryTree(int[] nums) {
          TreeNode root;
          if(nums.length==0)return null;
@@ -22,10 +32,11 @@ class Solution {
                 }
             }
             root=new TreeNode(Max);
+            //传数组加 form to会快一点
             root.left=constructMaximumBinaryTree(Arrays.copyOfRange(nums,0,index));
             root.right=constructMaximumBinaryTree(Arrays.copyOfRange(nums,index+1,nums.length));
          }
          return root;
     }
-    
+
 }
